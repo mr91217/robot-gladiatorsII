4f098999c19955df11b475ffaa12cba72f532b5f
ChunYu Chang <mr91217@gmail.com>
2024-01-08 13:18:16 -0500

    add fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("test test")
+//window.alert("test test")
+
+function fight(){
+    window.alert("Fight");
+}
+
+fight();
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("your name is " + playerName);
