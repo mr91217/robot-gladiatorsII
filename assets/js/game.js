a8712f69cdc003b199ed9b3435c78af2cfde2dc0
ChunYu Chang <mr91217@gmail.com>
2024-02-04 11:45:57 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,6 +19,13 @@ for(var i= 0; i < enemyNames.length; i++){
 }
 */
 
+// function to generate a random numeric value,random number 40-60
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max-min+1)+min);
+    
+    return value;
+};
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -35,9 +42,9 @@ console.log("Your name is "+playerName,",","Your HP is "+playerHealth,",",
             "Your Attack is "+playerAttack,".");
 
 
-var enemyNames = ["Lucas", "Momo", "Lolo"];
+var enemyNames = ["蓬蓬毛豆豆子", "Momo", "Lolo"];
 var enemyHealth = 50;
-var enemyAttack = 2;
+var enemyAttack = 10;
 
 //console.log("Enemy name is "+enemyNames,",","Enemy HP is "+enemyHealth,",",
 //            "Enemy Attack is "+enemyAttack,".");
@@ -59,7 +66,7 @@ var fight = function(enemyName){
         if (confirmSkip){
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
+            playerMoney = Math.max(0, playerMoney - 2);
             console.log("playermoney: "+playerMoney);
             break;
         }//else{
@@ -80,14 +87,15 @@ var fight = function(enemyName){
   // if player choses to fight, then fight
   //if (promptFight === "fight" || promptFight === "FIGHT"){
 
-  
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack-3, playerAttack);
 
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        enemyName + " now has " + enemyHealth + " health remaining." 
     );
 
     // check enemy's health
@@ -98,11 +106,13 @@ var fight = function(enemyName){
     }
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(playerName + " 給了 " + enemyName + "一頓胖揍. " + enemyName + "現在眼冒金星"
+        );
     }
 
-
+    var damage = randomNumber(enemyAttack-3, enemyAttack);
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -146,7 +156,7 @@ var startGame = function(){
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyName = enemyNames[i];
             // reset enemyHealth before starting new fight
-            enemyHealth = 20;
+            enemyHealth = randomNumber(40, 60);
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
             // if we're not at the last enemy in the array
