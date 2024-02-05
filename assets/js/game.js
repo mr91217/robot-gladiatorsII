d64091a1d0a9ab898044050bee90e289ae500029
ChunYu Chang <mr91217@gmail.com>
2024-02-05 10:43:26 -0500

    fix playername bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,9 +30,17 @@ var randomNumber = function (min, max) {
 //var playerHealth = 100;
 //var playerAttack = 10;
 //var playerMoney = 10;
+var getPlayerName = function(){
+    var name = "";
+    while(name === null || name === ""){
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+};
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -183,6 +191,7 @@ var startGame = function(){
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         if(playerInfo.health>0){
             window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+            
             // pick new enemy to fight based on the index of the enemy.names array
             //var pickedenemy.name = enemy.names[i];
             var pickedEnemyObj = enemyInfo[i];
